39d004df46f678fc59b7e4ff5a122860a3dd3191
rsps1008 <rsps1008@gmail.com>
2026-05-09 02:19:32 +0800

    調整widget預覽圖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.rsps1008.daymatter"
-        minSdk = 32
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
